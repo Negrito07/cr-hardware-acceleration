3f967c6a23a330c3e20c4ad6a10de49dae8f8d55
Gabriel <negrigabriel@hotmail.com>
2024-05-23 18:58:19 +0100

    init projet

---
+++ hardwareAcceleration/hardwareAcceleration.gen/sources_1/bd/mb_design/hdl/mb_design_wrapper.vhd
@@ -0,0 +1,55 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+--Date        : Thu May 23 18:37:44 2024
+--Host        : LAPTOP-VSCN0O2A running 64-bit major release  (build 9200)
+--Command     : generate_target mb_design_wrapper.bd
+--Design      : mb_design_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity mb_design_wrapper is
+  port (
+    dip_switches_16bits_tri_i : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dual_seven_seg_led_disp_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    led_16bits_tri_o : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    push_buttons_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    reset : in STD_LOGIC;
+    seven_seg_led_an_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+end mb_design_wrapper;
+
+architecture STRUCTURE of mb_design_wrapper is
+  component mb_design is
+  port (
+    dip_switches_16bits_tri_i : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    led_16bits_tri_o : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    push_buttons_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    dual_seven_seg_led_disp_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    seven_seg_led_an_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC;
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC
+  );
+  end component mb_design;
+begin
+mb_design_i: component mb_design
+     port map (
+      dip_switches_16bits_tri_i(15 downto 0) => dip_switches_16bits_tri_i(15 downto 0),
+      dual_seven_seg_led_disp_tri_o(7 downto 0) => dual_seven_seg_led_disp_tri_o(7 downto 0),
+      led_16bits_tri_o(15 downto 0) => led_16bits_tri_o(15 downto 0),
+      push_buttons_5bits_tri_i(4 downto 0) => push_buttons_5bits_tri_i(4 downto 0),
+      reset => reset,
+      seven_seg_led_an_tri_o(7 downto 0) => seven_seg_led_an_tri_o(7 downto 0),
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
+    );
+end STRUCTURE;
